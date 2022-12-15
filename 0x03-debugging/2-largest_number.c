16198fe1d3eb977cdfbdced30f7cc0164b3526e0
Transition2014 <kb.atoyebi@gmail.com>
2022-12-15 15:15:29 -0800

    my largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,11 +14,11 @@ int largest_number(int a, int b, int c)
 
 	if (a > b && a > c)
 	{
-	   largest = a;
+		largest = a;
 	}
 	else if (b > c && b > a)
 	{
-	        largest = b;
+		largest = b;
 	}
 	else if (c > b)
 	{
@@ -26,7 +26,7 @@ int largest_number(int a, int b, int c)
 	}
 	else
 	{
-	    largest = b;
+		largest = b;
 	}
 
 	return (largest);
