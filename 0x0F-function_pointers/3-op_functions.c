ccb659a6b05a8e9d5e8f6e4d96c78f451e7905e0
Transition2014 <kb.atoyebi@gmail.com>
2023-01-18 15:12:41 -0800

    my 3 project

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -3,24 +3,62 @@
 #include <stdlib.h>
 
 /**
- * get_op_func - a function to perform the operation asked by the user.
- * @s: operator
- * Return: pointer to corresponding operator.
+ * op_add - a function that adds a & b.
+ * @a: input a
+ * @b: input b
+ * Return: addition
  */
-
-int (*get_op_func(char *s))(int, int)
+int op_add(int a, int b)
 {
-	int (*ptr_fun)(int, int);
-
-	ptr_fun = *s == '+' ? op_add :
-		*s == '-' ? op_sub :
-		s == '' ? op_mul :
-		*s == '/' ? op_div :
-		*s == '%' ? op_mod : NULL;
-	if (!ptr_fun)
+	return (a + b);
+}
+/**
+ * op_sub - a function that subtracts a & b.
+ * @a: input a
+ * @b: input b
+ * Return: subtraction
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - a function that multiplies a & b.
+ * @a: input a
+ * @b: input b
+ * Return: multiplication
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - a function that divides a & b.
+ * @a: input a
+ * @b: input b
+ * Return: division
+ */
+int op_div(int a, int b)
+{
+	if (!b)
 	{
 		printf("Error\n");
-		exit(99);
+		exit(100);
 	}
-	return (ptr_fun);
+	return (a / b);
+}
+/**
+ * op_mod - a function that returns the modulo of a & b
+ * @a: input a
+ * @b: input b
+ * Return: modulo
+ */
+int op_mod(int a, int b)
+{
+	if (!b)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
 }
